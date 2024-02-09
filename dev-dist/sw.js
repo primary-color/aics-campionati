362668909aa61eb5b0f987cd8c4eaf495d1f3a54
Simone <sdiricco.91@gmail.com>
2024-02-09 21:02:42 +0100

    fix some issues

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b20ndrr6hq8"
+    "revision": "0.s44nj52prs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
