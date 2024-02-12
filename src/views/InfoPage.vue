c0515a08f45a889c447085256ddfb4df9020e0be
Simone <sdiricco.91@gmail.com>
2024-02-12 09:28:13 +0100

    add donate button

--- src/views/InfoPage.vue
+++ src/views/InfoPage.vue
@@ -7,8 +7,28 @@
       <Card class="mb-4">
         <template #content>
           <div class="flex flex-column align-items-center justify-content-center p-4">
-            <img :src="image" style="width: 65%" class="mb-4" />
+            <img :src="image" style="width: 100%" class="mb-4" />
             <div class="text-xl font-bold text-color-secondary">Versione App: {{ appVersion }}</div>
+          </div>
+
+          <div class="flex align-items-center justify-content-center w-full">
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+              <input type="hidden" name="business" value="6GTDV7EJ83LR6" />
+              <input type="hidden" name="amount" value="1" />
+              <input type="hidden" name="currency_code" value="EUR" />
+              <input
+                type="image"
+                src="https://www.paypalobjects.com/en_US/IT/i/btn/btn_donateCC_LG.gif"
+                border="0"
+                name="submit"
+                title="PayPal - The safer, easier way to pay online!"
+                alt="Donate with PayPal button" />
+              <img alt="" border="0" src="https://www.paypal.com/en_IT/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+          </div>
+
+          <div v-if="false">
+            <a href="https://www.paypal.com/donate/?business=6GTDV7EJ83LR6&amount=1&no_recurring=0&currency_code=EUR">Dona 1 Eur</a>
           </div>
         </template>
       </Card>
