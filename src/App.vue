8cd4394e1ac7e33ea974bfd822bc310fbbdb55bd
sdiricco <92826738+sdiricco@users.noreply.github.com>
2024-02-13 23:52:33 +0100

    Feat/add long loading (#5)
    * add long loading
    
    * add long loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import AppLongSpinner from "@/components/shared/AppLongSpinner.vue";
+import { useStore } from "@/store/main";
+
+const mainStore = useStore();
 </script>
 
 <template>
+  <AppLongSpinner :visible="mainStore.longLoading" />
   <RouterView />
 </template>
-
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
 export const TORNEO_API_BASE_URL = import.meta.env.VITE_APP_TORNEO_API_BASE_URL;
 
+export const REQUEST_DATE_MINUTES = 15;
+
 export const standingsLegend = {
   PT: "Punti totali",
   G: "Partite giocate",
