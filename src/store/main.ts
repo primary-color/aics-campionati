c7a245e23b0f02d56f778ccbdc3b0b8d3b7cb402
Simone <sdiricco.91@gmail.com>
2024-02-13 23:51:11 +0100

    add long loading spinner

--- src/store/main.ts
+++ src/store/main.ts
@@ -3,13 +3,7 @@ import * as vueuse from "@vueuse/core";
 import * as api from "@/api/api";
 import type { RemovableRef } from "@vueuse/core";
 import type { ITournamentDetails, IPlayerStats, ITournamentEntry, ICalendar } from "@/api/interfaces";
-import { DateTime } from "luxon";
 
-function checkLastRequestDate(requestDate: Date | undefined, minutes: number) {
-  const now = DateTime.now();
-  const lastRequestDate = (requestDate && DateTime.fromJSDate(new Date(requestDate))) || DateTime.now();
-  return now.diff(lastRequestDate, "minutes").minutes >= minutes;
-}
 interface IState {
   appVersion: string;
 
@@ -51,46 +45,26 @@ export const useStore = defineStore({
 
     //Fetch list of tournaments
     async fecthTournaments() {
-      if (checkLastRequestDate(this.requestDate, 1)) {
-        this.longLoading = true;
-      }
       const response = await api.getTournaments();
-      this.requestDate = new Date();
       this.tournaments = response.data.data;
-      this.longLoading = false;
     },
 
     //fetch tournament details
     async fecthTournamentDetails(id: string) {
-      if (checkLastRequestDate(this.requestDate, 1)) {
-        this.longLoading = true;
-      }
       const response = await api.getTournamentDetails(id);
-      this.requestDate = new Date();
       this.tournamentDetails = response.data.data;
-      this.longLoading = false;
     },
 
     //fetch players of tournament
     async fetchPlayers(id: string) {
-      if (checkLastRequestDate(this.requestDate, 1)) {
-        this.longLoading = true;
-      }
       const response = await api.getPlayersStats(id);
-      this.requestDate = new Date();
       this.playersStats = response.data.data;
-      this.longLoading = false;
     },
 
     //fetch tournament calendar
     async fetchTournamentCalendar(id: string, week?: number) {
-      if (checkLastRequestDate(this.requestDate, 1)) {
-        this.longLoading = true;
-      }
       const response = await api.getTournamentCalendar(id, week);
-      this.requestDate = new Date();
       this.tournamentCalendar = response.data.data;
-      this.longLoading = false;
     },
   },
 });
