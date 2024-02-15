2792127f5a39910463396e3ac688b221b91f2955
Simone <sdiricco.91@gmail.com>
2024-02-15 22:32:24 +0100

    add env mode to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import vue from "@vitejs/plugin-vue";
 
 import { VitePWA } from "vite-plugin-pwa";
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
+const name = process.env.NODE_ENV === "development" ? "Aics Lucca - campionati (beta)" : "Aics Lucca - campionati ";
+const short_name = process.env.NODE_ENV === "development" ? "AicsL (beta)" : "AicsL";
+const theme_color = process.env.NODE_ENV === "development" ? "#ff0000" : "#ffffff";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,10 +23,10 @@ export default defineConfig({
       injectRegister: "auto",
       includeAssets: ["futbol-regular-2.ico", "pwa-assets/ios/180.png"],
       manifest: {
-        name: "Aics Lucca - campionati",
-        short_name: "AicsL",
-        description: "Aics Lucca - campionati",
-        theme_color: "#ffffff",
+        name: name,
+        short_name: short_name,
+        description: "",
+        theme_color: theme_color,
         icons: [
           {
             src: "pwa-assets/android/android-launchericon-192-192.png",
