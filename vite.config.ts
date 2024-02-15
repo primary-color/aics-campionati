5fb0792cc041ae638a6a8380f073a6fa499c2b07
sdiricco <92826738+sdiricco@users.noreply.github.com>
2024-02-15 23:04:15 +0100

    Dev (#7)
    * Feat/add long loading (#5)
    
    * add long loading
    
    * add long loading spinner
    
    * add env mode to vite.config.ts
    
    * add isbeta

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,19 @@ import vue from "@vitejs/plugin-vue";
 
 import { VitePWA } from "vite-plugin-pwa";
 
+import dotenv from "dotenv";
+
+// Carica le variabili d'ambiente dal file .env
+dotenv.config();
+
+const IS_BETA = Boolean(process.env.IS_BETA);
+
+console.log("IS_BETA", IS_BETA);
+
+const name = IS_BETA ? "Aics Lucca - campionati (beta)" : "Aics Lucca - campionati ";
+const short_name = IS_BETA ? "AicsL (beta)" : "AicsL";
+const theme_color = IS_BETA ? "#ff0000" : "#ffffff";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,10 +30,10 @@ export default defineConfig({
       injectRegister: "auto",
       includeAssets: ["futbol-regular-2.ico", "pwa-assets/ios/180.png"],
       manifest: {
-        name: "Aics Lucca - campionati",
-        short_name: "AicsL",
-        description: "Aics Lucca - campionati",
-        theme_color: "#ffffff",
+        name: name,
+        short_name: short_name,
+        description: "",
+        theme_color: theme_color,
         icons: [
           {
             src: "pwa-assets/android/android-launchericon-192-192.png",
